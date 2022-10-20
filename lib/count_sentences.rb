bdddf4aa71f4aa0b461d6f683be3f41facf5294c
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-20 22:01:19 +0300

    task completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    return true 
+     else 
+    return false
+     end
   end
 
   def question?
-
+    if self.end_with?("?")
+    return true 
+    else
+      return false  
+  end
   end
 
   def exclamation?
+    if self.end_with?("!")
+    return true 
+    else
+      return false  
+  end
 
   end
 
   def count_sentences
-
+unless self.empty?
+  sum = self.split(/\.|\?|!/).reject {|str| str.empty?}.length
+  return sum
+   else
+    return 0
+   end
+  
   end
 end
